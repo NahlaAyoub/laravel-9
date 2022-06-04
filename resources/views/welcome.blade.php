fa5d342bfa2c067d3dafd41baa7f7d3f20aa78ba
NahlaAyoub <44461051+NahlaAyoub@users.noreply.github.com>
2022-06-04 02:45:20 +0200

    production update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,8 @@
                         </g>
                     </svg>
                 </div>
+                
+                <h1> This is Production Environment- Nahla Ayoub</h1>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
