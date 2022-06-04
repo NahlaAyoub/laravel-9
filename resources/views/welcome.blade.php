22d314325f739025be9bbbcf2769fc7542e17c7c
NahlaAyoub <44461051+NahlaAyoub@users.noreply.github.com>
2022-06-04 03:03:46 +0200

    update staging

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
                     </svg>
                 </div>
                 
-                <h1 style="text-align:center ;"> This is Production Environment- Nahla Ayoub</h1>
+                <h1 style="text-align:center ;"> This is Staging Environment- Nahla Ayoub</h1>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
